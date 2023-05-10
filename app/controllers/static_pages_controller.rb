da4e33b384ffda2829c718f7f4984f64ef58a120
Dr.Tec <56260809+tiwa2118@users.noreply.github.com>
2023-05-10 14:13:56 +0000

    Finish ch05

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -12,4 +12,8 @@ class StaticPagesController < ApplicationController
   def about
     # => app/views/static_pages/about.html.erb
   end
+
+  def contact
+    #=>  app/views/static_pages/contact.html.erb
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
+  get 'users/new'
   # GET / => static_pages#home
   root 'static_pages#home'
 
   # GET /static_pages/home => static_pages#home
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
+  # get 'static_pages/home'
+  # get 'static_pages/help'
+  # get 'static_pages/about'
+  # get 'static_pages/contact'
+  # get 'users/new'
+
+  get  "/help",    to: "static_pages#help"
+  get  "/about",   to: "static_pages#about"
+  get  "/contact", to: "static_pages#contact"
+  get  "/signup",  to: "users#new"
 end
